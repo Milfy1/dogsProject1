ab15c29722574a5ed670335e4469294a7f49f126
Milfy1 <adullah78@gmail.com>
2023-07-12 13:43:11 +0300

    non working yet , confluent connection (cloud)

--- src/main/java/com/udacity/bootstrap/BootstrapApplication.java
+++ src/main/java/com/udacity/bootstrap/BootstrapApplication.java
@@ -2,10 +2,19 @@ package com.udacity.bootstrap;
 //zeadoo
 //Mohammed-Aldrees
 //Tareq-Assiri
+import com.github.javafaker.Faker;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationStartedEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.kafka.core.KafkaTemplate;
+import reactor.core.publisher.Flux;
+
+import java.time.Duration;
+import java.util.stream.Stream;
 
 
 @SpringBootApplication
@@ -17,3 +26,19 @@ public class BootstrapApplication {
 	}
 
 }
+
+@RequiredArgsConstructor
+class Producer{
+	private final KafkaTemplate<Integer,String> template;
+
+	Faker faker;
+	@EventListener(ApplicationStartedEvent.class)
+	public void generate(){
+  faker = Faker.instance();
+		final Flux<Long> interval = Flux.interval(Duration.ofMillis(1_000));
+		final Flux<String> quotes = Flux.fromStream(Stream.generate(() -> faker.hobbit().quote()));
+		Flux.zip(interval,quotes).map( it -> template.send("spring-kafka", faker.random().nextInt(42), it.getT2())).blockLast();
+	}
+}
+
+
