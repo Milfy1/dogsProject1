ba452ded8ae597387d708e57948c87e6283a6e44
Milfy1 <adullah78@gmail.com>
2023-07-12 15:44:38 +0300

    Now working great , confluent connection (cloud)

--- src/main/java/com/udacity/bootstrap/BootstrapApplication.java
+++ src/main/java/com/udacity/bootstrap/BootstrapApplication.java
@@ -2,16 +2,15 @@ package com.udacity.bootstrap;
 //zeadoo
 //Mohammed-Aldrees
 //Tareq-Assiri
-import com.github.javafaker.Faker;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.event.ApplicationStartedEvent;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.event.EventListener;
 import org.springframework.kafka.core.KafkaTemplate;
-import reactor.core.publisher.Flux;
 
 import java.time.Duration;
 import java.util.stream.Stream;
@@ -22,23 +21,24 @@ import java.util.stream.Stream;
 public class BootstrapApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BootstrapApplication.class, args);
+		ApplicationContext ctx = SpringApplication.run(BootstrapApplication.class, args);
+		ctx.getBean(KafkaTemplate.class).send("MS.confluent", "not working");
 	}
 
 }
 
-@RequiredArgsConstructor
-class Producer{
-	private final KafkaTemplate<Integer,String> template;
-
-	Faker faker;
-	@EventListener(ApplicationStartedEvent.class)
-	public void generate(){
-  faker = Faker.instance();
-		final Flux<Long> interval = Flux.interval(Duration.ofMillis(1_000));
-		final Flux<String> quotes = Flux.fromStream(Stream.generate(() -> faker.hobbit().quote()));
-		Flux.zip(interval,quotes).map( it -> template.send("spring-kafka", faker.random().nextInt(42), it.getT2())).blockLast();
-	}
-}
+//@RequiredArgsConstructor
+//class Producer{
+//	private final KafkaTemplate<Integer,String> template;
+//
+//	Faker faker;
+//	@EventListener(ApplicationStartedEvent.class)
+//	public void generate(){
+//  faker = Faker.instance();
+//		final Flux<Long> interval = Flux.interval(Duration.ofMillis(1_000));
+//		final Flux<String> quotes = Flux.fromStream(Stream.generate(() -> faker.hobbit().quote()));
+//		Flux.zip(interval,quotes).map( it -> template.send("spring-kafka", faker.random().nextInt(42), it.getT2())).blockLast();
+//	}
+//}
 
 
