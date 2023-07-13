424226e23803c85b3a9877e526f640818ff32469
Tareq-Assiri <tareqassiri2000@gmail.com>
2023-07-13 15:12:49 +0300

    Added multiple topics.

--- src/main/java/com/udacity/bootstrap/KafkaConsumer/KafkaConsumer.java
+++ src/main/java/com/udacity/bootstrap/KafkaConsumer/KafkaConsumer.java
@@ -4,6 +4,9 @@ import com.udacity.bootstrap.deserializers.DeSerializer;
 import lombok.Setter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.messaging.handler.annotation.Header;
+import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -19,10 +22,10 @@ public class KafkaConsumer<T> {
     }
 
 
-    @KafkaListener(topics = "Dog", groupId = "your-group-id")
-    public void receiveMessage(byte[] message) {
+    @KafkaListener(topics = "Dog, Owner, AirTag", groupId = "your-group-id")
+    public void receiveMessage(@Payload byte[] message, @Header(KafkaHeaders.RECEIVED_TOPIC) String topic) {
         // Process the received Avro message
         T data = deSerializer.deserialize(tClass,message);
-        System.out.println("Received message: " + data.toString());
+        System.out.println("Received message: " +"topic: "+topic+ data.toString());
     }
 }
