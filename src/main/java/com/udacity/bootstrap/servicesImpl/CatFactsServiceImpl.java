1a5275d1ff62d818dff8164d8b51816c5c7f6e05
Tareq-Assiri <tareqassiri2000@gmail.com>
2023-07-17 10:55:47 +0300

    Added service notation to CatFactsServiceImpl

--- src/main/java/com/udacity/bootstrap/servicesImpl/CatFactsServiceImpl.java
+++ src/main/java/com/udacity/bootstrap/servicesImpl/CatFactsServiceImpl.java
@@ -1,8 +1,9 @@
 package com.udacity.bootstrap.servicesImpl;
 
 import com.udacity.bootstrap.services.CatFactsService;
+import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
-
+@Service
 public class CatFactsServiceImpl implements CatFactsService {
     @Override
     public String GetCatFact() {
