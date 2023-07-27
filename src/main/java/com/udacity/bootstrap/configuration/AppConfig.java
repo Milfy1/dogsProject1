559bb3d441079bdb811be7aa4533b0c8760831b9
zeadoo <ziad.almotlaq@hotmail.com>
2023-07-27 11:09:11 +0300

    added location attribute in the air tag entity as a whole and added the service, controller to return location from the air tag, also added the eureka client config.

--- src/main/java/com/udacity/bootstrap/configuration/AppConfig.java
+++ src/main/java/com/udacity/bootstrap/configuration/AppConfig.java
@@ -1,8 +1,10 @@
 package com.udacity.bootstrap.configuration;
 
 import org.modelmapper.ModelMapper;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
 
 @Configuration
 public class AppConfig {
@@ -10,5 +12,12 @@ public class AppConfig {
     @Bean
     public ModelMapper modelMapper() {
         return new ModelMapper();
+    }
+
+
+    @LoadBalanced
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
     }
 }
